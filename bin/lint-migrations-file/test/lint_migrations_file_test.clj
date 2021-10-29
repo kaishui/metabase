4b15e0dccb5335be4595d752c998d31d7c3e282e
Jeff Evans <jeff.evans@metabase.com>
2021-10-29 14:40:13 -0500

    Do the same logic for "blob" now, update rule and test
    Revert the previous bulk text type changes

--- bin/lint-migrations-file/test/lint_migrations_file_test.clj
+++ bin/lint-migrations-file/test/lint_migrations_file_test.clj
@@ -154,17 +154,18 @@
             {:sql {:dbms "postgresql,h2", :sql "2"}}]))))))
 
 (deftest prevent-text-types-test
-  (testing "should prevent \"text\" columns from being added after ID 320"
+  (testing "should allow \"${text.type}\" columns from being added"
     (is (= :ok
           (validate
            (mock-change-set
              :id 321
              :changes [(mock-add-column-changes :columns [(mock-column :type "${text.type")])]))))
-    (is (thrown-with-msg?
-          clojure.lang.ExceptionInfo
-          #"(?s)^.*no-bare-text-types\\?.*$"
-          (validate
-            (mock-change-set
-              :id 321
-              :changes [(mock-add-column-changes :columns [(mock-column :type    "text"
-                                                                        :remarks "New text column")])]))))))
+    (doseq [problem-type ["blob" "text"]]
+      (testing (format "should prevent \"%s\" columns from being added after ID 320" problem-type)
+        (is (thrown-with-msg?
+              clojure.lang.ExceptionInfo
+              #"(?s)^.*no-bare-blob-or-text-types\\?.*$"
+              (validate
+                (mock-change-set
+                  :id 321
+                  :changes [(mock-add-column-changes :columns [(mock-column :type    problem-type)])]))))))))
