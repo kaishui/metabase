bbd13346369fc1d071618db457031b3b49206591
Noah Moss <noahbmoss@gmail.com>
2021-10-29 15:18:25 -0400

    WIP event sending

--- src/metabase/util/analytics.clj
+++ src/metabase/util/analytics.clj
@@ -4,7 +4,9 @@
            [com.snowplowanalytics.snowplow.tracker.emitter BatchEmitter Emitter]
            com.snowplowanalytics.snowplow.tracker.http.ApacheHttpClientAdapter
            org.apache.http.impl.client.HttpClients
-           org.apache.http.impl.conn.PoolingHttpClientConnectionManager))
+           org.apache.http.impl.conn.PoolingHttpClientConnectionManager
+           com.snowplowanalytics.snowplow.tracker.payload.SelfDescribingJson
+           com.snowplowanalytics.snowplow.tracker.events.Unstructured))
 
 (def ^:private ^Emitter emitter
   "An instance of a Snowplow emitter"
@@ -18,10 +20,22 @@
                     (.build))]
     (-> (. BatchEmitter builder)
         (.httpClientAdapter adapter)
-        (.bufferSize 5)
+        (.bufferSize 1) ;; TODO bump buffer size back to 5?
         (.build))))
 
 (def ^:private ^Tracker tracker
   "An instance of a Snowplow tracker"
   (-> (Tracker$TrackerBuilder. emitter "sp" "metabase")
     (.build)))
+
+(defn- payload
+  []
+  (let [event-data (doto (new java.util.HashMap)
+                     (.put "event" "database_selected")
+                     (.put "version" "1.0.0")
+                     (.put "database" "test"))]
+    (new SelfDescribingJson "iglu:com.metabase/setup/jsonschema/1-0-1" event-data)))
+
+(comment (.track tracker (-> (. Unstructured builder)
+                             (.eventData (payload))
+                             (.build))))
